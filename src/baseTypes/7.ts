f11632b6ed3e6da1f3fa160687c430d799425a8b
Alex Kydanov <a.kidanov@gmail.com>
2024-06-22 23:41:17 +0300

    HW06

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,23 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum Weekday {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+function isWeekend(day: Weekday): boolean {
+  switch(day) {
+    case Weekday.Saturday:
+    case Weekday.Sunday:
+      return true;
+    default:
+      return false;
+  }
+}
--- src/generic/1.ts
+++ src/generic/1.ts
@@ -1,17 +1,18 @@
 /*
-  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа. 
+  Є функція getPromise(), яка повертає проміс, що дозволяється в масив, що містить рядки та числа.
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+function getPromise(): Promise<Array<string | number>> {
   return new Promise((resolve) => {
     resolve(['Text', 50]);
   });
 }
 
 getPromise()
-.then((data) => {
-  console.log(data);
-});
+  .then((data) => {
+    console.log(data);
+  });
+
 
 export {};
